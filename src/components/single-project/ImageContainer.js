cd3ec1044b9530b900bd7a8b680309baa7982f69
Christian Coleman <christian.c2508@gmail.com>
2018-12-28 00:14:14 -0500

    positioned items on grid, added image

--- src/components/single-project/ImageContainer.js
+++ src/components/single-project/ImageContainer.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './styles/ImageContainer.css';
+import portrait from '../../assets/CoryPictures/portaitOne.jpg';
+
+// Dynamically adding in images 
+const imgStyle = {
+    width: '100%',
+    backgroundImage: `url(${portrait})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '50% 50%',
+};
 
 export default class ImageContainer extends Component {
+
   render() {
     return (
-      <div className="ImageContainer-container">
-        <h1>ImageContainer</h1>
+      <div className="ImageContainer-container" style={imgStyle}>
+        {/* <div className="ImageContainer-img" style={{imgStyle}}></div> */}
+        {/* <img src={require("../../assets/CoryPictures/portaitOne.jpg")} alt="photo one" style={{ height: '100%', width: '100%' }}/> */}
       </div>
     )
   }
